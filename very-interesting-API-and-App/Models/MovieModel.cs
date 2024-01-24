dd6870f6402938ce325250caf18d509b6cfe42d6
EmmaDQ <sydemma@hotmail.com>
2024-01-24 15:31:10 +0100

    second commit, get git to work

---
+++ very-interesting-API-and-App/Models/MovieModel.cs
@@ -0,0 +1,11 @@
+﻿namespace very_interesting_API_and_App.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string? ImageLink { get; set; }
+    }
+}
